4a8a80e1b377a124fb666bbb76f12f7ee5d2016e
Per Øyvind Håvelsrud <peroyvind@havelsrud.com>
2020-05-19 17:08:30 +0200

    Update chocoapps.ps1
    Removed packages I don't use and add my own packages.

--- chocoapps.ps1
+++ chocoapps.ps1
@@ -1,4 +1,4 @@
-# Install script for Kristian Alnaes
+# Install script for Per Øyvind, adapted from https://github.com/kristianalnaes/chocoinstallscripts
 
 # Created 06.08.2015
 
@@ -8,54 +8,42 @@ ECHO Configure chocolatey
 
 choco feature enable -n allowGlobalConfirmation
 
-choco install jre8
-choco install jdk8
-choco install curl
-choco install ruby
-choco install yeoman 
-choco install atom
 choco install visualstudiocode
-choco install virtualbox
-choco install nodejs.install
-choco install github 
 choco install python
-choco install conemu
 choco install azcopy
 choco install wireshark
 choco install azure-cli
-choco install windirstat
 choco install fiddler4
-choco install steam
-choco install phantomjs
+choco install linqpad
 choco install docker
 choco install jq
 choco install slack
-choco install emacs
-choco install racket
-choco install visualfsharptools
-
 
 # DEVOPS
 
-
-choco install filezilla 
+choco install winscp
 
 choco install putty 
 
+#Sourcecontrol
 
+choco install git
+choco install gitextensions
 
 #Misc
 
 choco install 7zip 
-choco install git
 choco install skype
-
+choco install kdiff3
+choco install postman
 
 # Browsers
 
-choco install google-chrome-x64
+choco install googlechrome
 
+# Password managers
 
+choco install bitwarden
 
 
 choco feature disable -n allowGlobalConfirmation
